0dbce49a8f5323a69d264690e85ad0964f287c7f
drzln <luis.d@akeyless.io>
2026-06-08 18:07:55 -0700

    flake: follow substrate/nixpkgs (fleet nixpkgs alignment for cache reuse)

--- flake.nix
+++ flake.nix
@@ -3,10 +3,9 @@
   description = "Redis-backed session management with device fingerprinting";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
+    nixpkgs.follows = "substrate/nixpkgs";
     substrate = {
       url = "github:pleme-io/substrate";
-      inputs.nixpkgs.follows = "nixpkgs";
     };
     crate2nix = {
       url = "github:nix-community/crate2nix";
